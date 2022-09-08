5213a6b8e12990588e778a4fbf4834e21d09c038
wichis <wichiz1312@gmail.com>
2022-09-08 00:03:26 -0500

    Initial commit

---
+++ src/test/java/mx/ryo/xolioit/auth/conf/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.ryo.xolioit.auth.conf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
